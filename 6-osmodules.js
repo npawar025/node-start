9eb5e1055afe55c894d5b59ea2c744784b9a3b22
Nilesh <npawar025@gmail.com>
2023-06-02 11:57:46 +0530

    new update

--- 6-osmodules.js
+++ 6-osmodules.js
@@ -6,3 +6,4 @@ console.log(os.freemem());
 console.log(os.totalmem());
 console.log(os.type());
 console.log(os.release());
+console.log(os.uptime());
---
+++ 9-fsmodule-async.js
@@ -0,0 +1,28 @@
+//we need callback as all async functions
+const { readFile, writeFile } = require("fs");
+
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile("./content/second.txt", "utf8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile(
+      "./content/result-async.txt",
+      `Here is result: ${first} and ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result);
+      }
+    );
+  });
+});
